abb1559852b6f7b5bce15576745da93818bdbea3
Pilina <tkachuk_polina@bk.ru>
2022-04-29 13:49:32 +0700

    Testing the program in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,28 @@
 #include <QCoreApplication>
+#include <iostream>
+#include "Subject.h"
 
-int main(int argc, char *argv[])
-{
-    QCoreApplication a(argc, argv);
+#include <chrono>
+#include <thread>
 
-    return a.exec();
+using namespace std;
+//Задание:На основе паттерна Наблюдателя, написать программу для слежения за состоянием выбранного файла
+//Для тестирования программы, создала файл в папке поекта- "test.txt"
+//все работает корректно при 3х описанных ситуациях
+int main() {
+    //В главной программе создаем объект для отслеживания состояния конкретного файла
+    FileMonitor monitor( "test.txt" );
+    //затем определяется объект наблюдатель
+    FileObserver observer;
+    // после чего связываем Наблюдателя с Источником
+    monitor.Attach( &observer );
+
+    while( true ) {
+        monitor.checkFile();
+        //использование бесконечного цикла, в котором будем обновлять состояние объекта
+        //Источника каждые 100 миллисекунд
+        this_thread::sleep_for( chrono::milliseconds( 100 ) );
+    }
+
+    return 0;
 }
